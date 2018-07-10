6d2926b6e2149f3b62bd08a6403121d0e03e9fac
Narthana Epa <triarius@users.noreply.github.com>
2018-07-10 15:05:43 +1000

    avoid printing usage and repeating error on error

--- cmd/root.go
+++ cmd/root.go
@@ -37,6 +37,8 @@ examples and usage of using your application. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
+	SilenceErrors: true,
+	SilenceUsage:  true,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
